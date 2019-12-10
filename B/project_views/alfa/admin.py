45d7e81ae0b0b837ef00d28ed8e552962feb32a5
ilham faturachman <faturachman@alterra.id>
2019-12-10 23:08:10 +0700

    Upload perbaikan

--- B/project_views/alfa/admin.py
+++ B/project_views/alfa/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from.models import Mentee, Mantor, Blog
 
 # Register your models here.
-
-admin.site.register(Mentee)
-admin.site.register(Mentor)
-admin.site.register(Blog)
